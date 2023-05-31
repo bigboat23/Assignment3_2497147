159627aa3414b9de937d539040495f5e8b7dd244
bigboat23 <117849391+bigboat23@users.noreply.github.com>
2023-05-31 20:48:55 +0200

    BlogPageUpdate

--- script.js
+++ script.js
@@ -83,4 +83,11 @@ function getBlogContent(week) {
   
 }
 
+const darkModeButton = document.getElementById('darkModeButton');
+const body = document.body;
+
+darkModeButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
+
 
