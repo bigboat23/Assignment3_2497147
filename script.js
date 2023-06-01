fab2b8cb3051805dc9805717d133ed98a019a1f8
bigboat23 <117849391+bigboat23@users.noreply.github.com>
2023-06-01 16:48:51 +0200

    PortfolioChanges

--- script.js
+++ script.js
@@ -83,11 +83,20 @@ function getBlogContent(week) {
   
 }
 
-const darkModeButton = document.getElementById('darkModeButton');
-const body = document.body;
+//Track MouseCLick
 
-darkModeButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-});
+const track= document.getElementById("image-track");
+window.onmousedown = e =>{
+if(track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+         maxDelta = window.innerWidth/2;
+
+  const percentage = (mouseDelta/ maxDelta)*100;
+        nextPercentage = parseFloat(track.dataset.prevPercentage)+percentage;
+
+        track.dataset.percentage= nextPercentage;
+        track.style.transform  = 'translate(${percentage}%,-50%)';
+}
 
 
